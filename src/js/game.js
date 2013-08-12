bce26304ca55d7ac7cb04a8f6b75471daf42a4d7
Jim R. Wilson <wilson.jim.r@gmail.com>
2013-08-12 00:11:18 -0400

    implementing size and difficulty selection

--- src/js/game.js
+++ src/js/game.js
@@ -6,7 +6,25 @@
 var
   
   // hex game
-  hexmines = window.hexmines = {},
+  hexmines = window.hexmines = {
+    
+    // size string to value mappings
+    sizeValues: {
+      small: 4,
+      medium: 6,
+      large: 8,
+      enormous: 12
+    },
+    
+    // difficulty string to density mappings
+    difficultyValues: {
+      easy: 0.05,
+      medium: 0.15,
+      hard: 0.25,
+      terrifying: 0.5
+    }
+    
+  },
   
   /**
    * visit the immediate neighbors of the selected tile.
@@ -79,8 +97,80 @@ var
       
       game = {
         
+        // initialize game
+        init: function() {
+          
+          var
+            
+            tileCount = 0,
+            mineCount = 0,
+            
+            size = hexmines.sizeValues[game.size],
+            difficulty = hexmines.difficultyValues[game.difficulty];
+          
+          game.steps = 0;
+          
+          game.area = hexmines.createArea(size),
+          game.areaKeys = Object.keys(game.area),
+          
+          mines = game.mines = {};
+          
+          visitArea(game.area, function(x, y) {
+            var
+              $tile = $('<div class="tile unexplored"></div>'),
+              inv = grid.screenpos(x, y);
+            $tile.css({
+              left: inv.x,
+              top: inv.y
+            });
+            $tile.appendTo(grid.root);
+            tiles[hex.key(x, y)] = $tile;
+            tileCount += 1;
+          });
+          
+          game.$cursor.appendTo(grid.root);
+          
+          // add mines until desired difficulty has been achieved
+          while (mineCount / tileCount < difficulty) {
+            console.log(mineCount / tileCount, difficulty);
+            game.addMine();
+            mineCount += 1;
+          }
+          
+          game.center();
+          
+        },
+        
+        // reset the game
+        reset: function() {
+          
+          var key;
+          
+          // clear out existing mines
+          mines = game.mines = {};
+          
+          // remove tiles
+          for (key in tiles) {
+            tiles[key].remove();
+          }
+          tile = game.tiles = {};
+          
+          // reinitialize
+          game.init();
+          
+        },
+        
+        // reorient to center
+        center: function() {
+          var $elem = $(game.grid.elem);
+          game.grid.reorient($elem.width() * 0.5, $elem.height() * 0.5);
+        },
+        
         // number of steps taken
         steps: 0,
+        
+        // mines
+        mines: null,
         
         /**
          * add a mine to the minefield
@@ -94,8 +184,8 @@ var
           while (!done) {
             index = (Math.random() * areaKeys.length) << 0;
             key = areaKeys[index];
-            if (!(key in mines)) {
-              mines[key] = 1;
+            if (!(key in game.mines)) {
+              game.mines[key] = 1;
               done = true;
             }
           }
@@ -106,6 +196,10 @@ var
          */
         step: function(x, y) {
           
+          if (!game.region.inside(x, y)) {
+            return;
+          }
+          
           game.steps += 1;
           
           var
@@ -113,14 +207,14 @@ var
             queue = [[x, y]],
             visited = {};
           
-          if (key in mines) {
+          if (key in game.mines) {
             if (game.steps === 1) {
               // first step was on a mine, find a new home for it and continue
               game.addMine();
-              delete mines[key];
+              delete game.mines[key];
             } else {
               // sorry, a loser is you :(
-              visitArea(area, game.reveal);
+              visitArea(game.area, game.reveal);
               tiles[key].attr('class', 'tile hit');
               return;
             }
@@ -144,12 +238,8 @@ var
             }).apply(null, queue.shift());
           }
           
-          // check to see if this is the end game
-          var $unexplored = $(grid.root).find('.unexplored');
-          console.log($unexplored.length);
-          if ($unexplored.length === Object.keys(mines).length) {
-            $unexplored.attr('class', 'tile safe');
-          }
+          game.checkWin();
+          
           
         },
         
@@ -166,43 +256,46 @@ var
             return;
           }
           
-          if (key in mines) {
+          if (key in game.mines) {
             return tiles[key].attr('class', 'tile mine');
           }
           
           count = 0;
           visitNeighbors(x, y, function(x, y) {
-            count += +(hex.key(x, y) in mines);
+            count += +(hex.key(x, y) in game.mines);
           });
           tiles[key].attr('class', 'tile n' + count);
           
+        },
+        
+        checkWin: function() {
+          // check to see if this is the end game
+          var $unexplored = $(game.grid.root).find('.unexplored');
+          if ($unexplored.length === Object.keys(game.mines).length) {
+            $unexplored.attr('class', 'tile safe');
+          }
         }
         
       },
       
       grid = game.grid = config.grid,
-      size = +config.size || 5,
       
       // cursor to indicate mouse/touch location
       $cursor = game.$cursor =
         $('<div class="tile cursor" style="display:none"></div>'),
       
-      // game area
-      area = game.area = hexmines.createArea(5),
-      areaKeys = game.areaKeys = Object.keys(area),
-      
       // collection of jQuery result objects, one for each tile
       tiles = game.tiles = {},
-      
-      // collection of mines
-      mines = {},
       
       // region to match area
       region = game.region = hex.region(grid, {
         inside: function(x, y) {
-          return (hex.key(x, y) in area);
+          return (hex.key(x, y) in game.area);
         }
       });
+    
+    game.size = config.size || 'small',
+    game.difficulty = config.difficulty || 'easy',
     
     // move the cursor
     grid.addEvent("tileover", function(e, x, y) {
@@ -223,29 +316,8 @@ var
     grid.addEvent("tiletap", function(e, x, y) {
       game.step(x, y);
     });
-
-    visitArea(area, function(x, y) {
-      var
-        $tile = $('<div class="tile unexplored"></div>'),
-        inv = grid.screenpos(x, y);
-      $tile.css({
-        left: inv.x,
-        top: inv.y
-      });
-      $tile.appendTo(grid.root);
-      tiles[hex.key(x, y)] = $tile;
-    });
     
-    $cursor.appendTo(grid.root);
-    
-    // experimenting, creating mines and setting tiles accordingly
-    (function(){
-      
-      for (var i = 0; i < 10; i++) {
-        game.addMine();
-      }
-      
-    })();
+    game.init();
     
     return game;
     
--- src/js/main.js
+++ src/js/main.js
@@ -7,18 +7,54 @@ hex.debug = true;
 
 var
   
-  // hex grid for background effect
-  $outer = $('.mines-outer'),
-  $inner = $outer.find('.mines-inner'),
-  grid = hex.grid($outer[0]),
+  /**
+   * convenience methods for working with local storage
+   */
+  stor = window.localStorage,
+  get = function(key) {
+    try {
+      return JSON.parse(stor.getItem('hm-' + key));
+    } catch (err) {
+      return null;
+    }
+  },
+  set = function(key, val) {
+    return stor.setItem('hm-' + key, JSON.stringify(val));
+  },
   
-  // create game
-  game = hexmines.createGame({
-    grid: grid,
-    size: 5
-  });
+  // get cached size and difficulty values
+  size = get('size') || 'small',
+  difficulty = get('difficulty') || 'easy',
+  
+  // instance of hexmines game
+  game;
+
+$('input[name="size"]')
+  .change(function(event) {
+    game.size = $(this).val();
+    set('size', game.size);
+    game.reset();
+  })
+  .filter('[value="' + size + '"]')
+    .prop('checked', true);
+
+// check appropriate size/difficulty boxes
+$('input[name="difficulty"]')
+  .change(function(event) {
+    game.difficulty = $(this).val();
+    set('difficulty', game.difficulty);
+    game.reset();
+  })
+  .filter('[value="' + difficulty + '"]')
+    .prop('checked', true);
+
+game = hexmines.createGame({
+  grid: hex.grid($('.minefield')[0]),
+  size: size,
+  difficulty: difficulty
+});
 
 // reorient so the root is centered in the grid element
-grid.reorient($inner.width() * 0.5, $inner.height() * 0.5);
+game.center();
 
 })(window, hexmines, hex, jQuery);
